35df950b9ea312eb3c6e2cb21a7649b2a586fc66
Lucas Thaw <saiwaizinthaw@gmail.com>
2024-05-03 01:20:51 +0700

    "fix bugs"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { Route, Routes, useRoutes } from "react-router-dom";
 import "./App.css";
-import Layout from "./Layout";
+import { UserRouter } from "./routers";
 
 function App() {
+  const UserRouting = useRoutes(UserRouter);
+
   return (
-    <div className="w-full min-h-screen">
-      <Layout />
-    </div>
+    <>
+      <div>
+        <Routes>
+          <Route path="/*" element={UserRouting} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
---
+++ src/routers/UserRouter.tsx
@@ -0,0 +1,16 @@
+import { RouteObject } from "react-router-dom";
+import { Home, ProductDetails } from "../pages";
+
+const UserRouter: RouteObject[] = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+
+  {
+    path: "/product/:id",
+    element: <ProductDetails />,
+  },
+];
+
+export default UserRouter;
