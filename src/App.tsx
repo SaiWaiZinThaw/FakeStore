4b40887df64a9dd8cff5e69495c47743d69f410e
Lucas Thaw <saiwaizinthaw@gmail.com>
2024-05-03 01:47:52 +0700

    fix comflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { Route, Routes, useRoutes } from "react-router-dom";
+import { Route } from "react-router-dom";
 import "./App.css";
 import { UserRouter } from "./routers";
+import { Routes, useRoutes } from "react-router";
 
 function App() {
   const UserRouting = useRoutes(UserRouter);
